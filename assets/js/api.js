9e1c32581f0264a98e76c3d4ddf30bd36ab300c9
Vítor Hugo Tavares Moura <vitorhugotavaresmoura@gmail.com>
2025-02-07 14:50:17 -0300

    primeiro commit

--- assets/js/api.js
+++
@@ -1,7 +0,0 @@
-
-
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/meuusurario/js-developer-portfolio/refs/heads/main/data/profile.json';
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-
-
-(async () => {
-
-    const profileData = await fetchProfileData()
-    console.log(profileData)
-})()
